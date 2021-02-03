2eb22776d0ac18c22c02b5b3e889e98ef59261ea
joshsrai <joshsrai@gmail.com>
2021-02-03 16:44:53 -0500

    commit tree e4740dc278dbf71bfd86edb4992209c4ebf4a5f2

--- index.js
+++ index.js
@@ -3,3 +3,13 @@ const testVar = {}
 function testFunc() {
   return "hi"
 }
+function superbowlWin(array) {
+  let find = array.find(function(obj) {return obj.result === "W"})
+
+  if (find) {
+    return find.year
+  }
+  else {
+    undefined
+  }
+}
